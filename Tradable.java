af487834fbb4c043c745711ddc1d2268c0ab6605
Daryl <daryltjl@gmail.com>
2015-04-21 10:12:56 -0500

    Added tradable class (not complete)

---
+++ Tradable.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package SE350;
+
+/**
+ *
+ * @author Daryl
+ */
+public class Tradable {
+    String getProduct(){
+        
+    }
+    
+    Price getPrice(){
+        
+    }
+    
+    int getOriginalVolume(){
+        
+    }
+    
+    int getRemainingVolume(){
+        
+    }
+    
+    in getCancelledVolume(){
+        
+    }
+    
+    void setCancelledVolume(int newCancelledVolume){
+        
+    }
+    
+    void setRemainingVolume(int newRemainingVolume){
+        
+    }
+    
+    String getUser(){
+        
+    }
+    
+    String getSide(){
+        
+    }
+    
+    boolean isQuote(){
+        
+    }
+    
+    String getId(){
+        
+    }
+}
+
