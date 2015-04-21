fd30c510b865fde59d5881252ba8e392041cfd28
jayodele <jerryayodele@me.com>
2015-04-21 18:34:02 -0500

    Order class, look at it

--- Order.java
+++ Order.java
@@ -6,25 +6,48 @@ package SE350;
  * @author Daryl
  */
 public class Order {
+	
+	String onePartOfID;
+	String time_nano;
     String product;
     int OriQuantity; 
     int RemQuantity;
     int CanQuantity;
     String UserID;
-    String side; 
+    String b_side; 
     boolean quote = false;
     String ID; 
+    Price PriceOrder = new Price();
     Price Tradable = new Price();
+
     
     public void Order(String userName, String productSymbol, Price orderPrice, int originalVolume, Bookside side)
     {
         UserID = userName; 
-        ID = productSymbol;
-        //orderPrice - don't know what to do with it
+        product = productSymbol;
+        b_side = side;
         OriQuantity = originalVolume; 
-        //Book side
-        
-        //test git
-        // this is jerry
+        time_nano = Long.toString(System.nanoTime());
+        PriceOrder = orderPrice;
+        onePartOfID = PriceOrder.toString();
+        ID = UserID + product + onePartOfID + time_nano;
     }
+    
+   public String getProduct() {
+	   return this.product;
+   }
+   
+   public int getRemainingVolume(){
+	   return this.RemQuantity;
+   }
+   
+   public void setRemainingVolume(int newRemainingVolume) {
+	   RemQuantity = newRemainingVolume;
+   }
+   
+   public String toString() {
+	   String sum_string;
+	   sum_string = UserID + "order:" + b_side + " " + OriQuantity + " " + '$'+ PriceOrder + "( Original Vol: " + OriQuantity + ", CXL'd Vol: " + CanQuantity + "), ID: " + ID;
+	   return sum_string;
+   }
 }
