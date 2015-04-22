0842c473dc4e4486756276849aa19310fa05309b
Daryl <daryltjl@gmail.com>
2015-04-22 13:09:05 -0500

    quote class update

--- Quote.java
+++ Quote.java
@@ -7,10 +7,29 @@ public class Quote {
     String StockSymbol;
     QuoteSide Buy = new QuoteSide();
     QuoteSide Sell = new QuoteSide();
-    
+    Price PriceOrder = new Price();
     
     public void Quote(String userName, String productSymbol, Price buyPrice,
             int buyVolume, Price sellPrice, int sellVolume){
+        UserName = userName; 
+        StockSymbol = productSymbol;
+        PriceOrder = buyPrice; 
+        Buy(UserName, StockSymbol, PriceOrder, buyVolume, bookSide); //bookside not created yet.
+    }
+    
+    public String getUserName(){
+        return UserName;
+    }
+    
+    public String getProduct(){
+        return StockSymbol;
+    }
+    
+    public QuoteSide getQuoteSide(Bookside sideIn){
+        //work on this
+    }
+    
+    public String toString(){
         
     }
 }
