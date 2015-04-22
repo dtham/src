53c37e9b7fc0b94b4015901df76e9b47b7f5d8ca
Daryl <daryltjl@gmail.com>
2015-04-22 13:00:14 -0500

    tradableDTO

--- Quote.java
+++ Quote.java
@@ -3,5 +3,14 @@ package SE350;
 
 
 public class Quote {
+    String UserName;
+    String StockSymbol;
+    QuoteSide Buy = new QuoteSide();
+    QuoteSide Sell = new QuoteSide();
     
+    
+    public void Quote(String userName, String productSymbol, Price buyPrice,
+            int buyVolume, Price sellPrice, int sellVolume){
+        
+    }
 }
